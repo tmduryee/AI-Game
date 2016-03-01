2d7a8b713f0f6d7f35871e80e600d0b6092ffd4e
TYLER DURYEE (RIT Student) <tmd6246@ad.rit.edu>
2016-03-01 16:44:33 -0500

    Replaced cat and dog models, added SmoothFollow script to camera so the camera now follows the player (Cat).

---
+++ Project1/Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float distance = 10.0f;
+    public float height = 5.0f;
+    public float heightDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+
+	void LateUpdate ()
+    {
+        // early out if we don't have a target
+        if (!target)
+            return;
+
+        float wantedRotationAngle = target.eulerAngles.y;
+        float wantedHeight = target.position.y + height;
+        float currentRotationAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
+
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        // convert angle to rotation
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        //  Always look at target
+        transform.LookAt(target);
+    }
+}
