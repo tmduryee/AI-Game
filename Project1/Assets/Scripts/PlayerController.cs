b500d4e54d029a2b8488170f4a4c2b3d57c9de81
TYLER DURYEE (RIT Student) <tmd6246@ad.rit.edu>
2016-03-09 15:51:04 -0500

    Updated character controller and cat and dog prefabs, player can use mouse to look around now.

--- Project1/Assets/Scripts/PlayerController.cs
+++ Project1/Assets/Scripts/PlayerController.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    float playerSpeed = 1.0f;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public float playerSpeed = 1.0f;
+	public float rotateSpeed = 3.0f;
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 	    if (Input.GetKey(KeyCode.W)) transform.Translate(new Vector3(0, 0, playerSpeed));
-        if (Input.GetKey(KeyCode.A)) transform.Translate(new Vector3(-playerSpeed, 0, 0));
-        if (Input.GetKey(KeyCode.S)) transform.Translate(new Vector3(0, 0, -playerSpeed));
-        if (Input.GetKey(KeyCode.D)) transform.Translate(new Vector3(playerSpeed, 0, 0));
+        if (Input.GetKey(KeyCode.A)) transform.Translate(new Vector3(-playerSpeed/2, 0, 0));
+        if (Input.GetKey(KeyCode.S)) transform.Translate(new Vector3(0, 0, -playerSpeed/2));
+        if (Input.GetKey(KeyCode.D)) transform.Translate(new Vector3(playerSpeed/2, 0, 0));
 
+		// Character rotation using mouse
+		float forward = Input.GetAxis("Vertical");
+		float side = Input.GetAxis("Horizontal");
+		float rotY = Input.GetAxis("Mouse X");
+
+		gameObject.transform.Rotate(0, rotY * rotateSpeed, 0);
     }
 }
